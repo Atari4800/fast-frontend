9113983c1161bb65e660244f5f07188aac2cc5eb
Jake Peterson <60277102+Jake152@users.noreply.github.com>
2022-03-13 16:32:05 -0500

    Syntax fixes in RouteResults.js
    Changes from == to === or != to !==

--- frontend/src/components/Route_Results/RouteResults.js
+++ frontend/src/components/Route_Results/RouteResults.js
@@ -98,7 +98,7 @@ getRecipientComment(recipient) {
     let clients = this.state.recipients	
     for (let i = 0; i < clients.length; i++) {	
         if (clients[i].id === recipient.id) {	
-            if (clients[i].comments != undefined) {	
+            if (clients[i].comments !== undefined) {	
                 return clients[i].comments	
             }	
             else {	
@@ -118,7 +118,7 @@ getRecipientRoomNumber(recipient) {
     let clients = this.state.recipients	
     for (let i = 0; i < clients.length; i++) {	
         if (clients[i].id === recipient.id) {	
-            if (clients[i].location.room_number != undefined && clients[i].location.room_number != "N/A") {	
+            if (clients[i].location.room_number !== undefined && clients[i].location.room_number !== "N/A") {	
                 return clients[i].location.room_number	
             }	
             else {	
