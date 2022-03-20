bcacb2b9ffc81cda908b116cd6270132cf27c2da
Nick Cox <nbcox@unomaha.edu>
2022-03-20 15:02:40 -0500

    Modified to print a single route in History

--- frontend/src/components/History/History.js
+++ frontend/src/components/History/History.js
@@ -1,143 +1,135 @@
-import RouteService from '../../services/RouteService';
-import  React, { Component } from  'react';
+import React, { Component } from 'react';
+
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
+import RouteService from '../../services/RouteService'
 import RecipientService from '../../services/RecipientService'
 import DriverService from '../../services/DriverService'
-const routeService = new RouteService;
 
+const recipientService = new RecipientService();
+const routeService = new RouteService();
+const driverService = new DriverService();
+
+
+/**
+ * This component is used to display route information for an 
+ * individual driver.
+ */
 class History extends Component {
 
+    /**
+     * The constructor method initializes the component's state object and
+     * binds the methods of the component to the current instance.
+     * @param {Object} props The properties passed to the component.
+     */
     constructor(props) {
         super(props);
-        const {id} = props.match.params;
-        this.state  = {
-            routeList: {id: id, routes:[], others:[]},
+        this.state = {
+            route: {
+                itinerary: [], total_distance: "", total_duration: "",
+                total_quantity: "", first_name: "", last_name: ""
+            },
             recipients: [],
-            drivers: [],
+            driver: { capacity: "", employee_status: "" }
         };
 
-        this.displayAllRoutes = this.displayAllRoutes.bind(this);
-        this.displayAnyRoute = this.displayAnyRoute.bind(this);
+        this.getRecipientName = this.getRecipientName.bind(this)
+        this.getPhone = this.getPhone.bind(this)
     }
 
+    /**
+     * Life cycle hook that is called after the component is first rendered.
+     */
     componentDidMount() {
-        routeService.getRouteList().then(result => {
-            console.log(result)
+        let params = this.props.match.params
+
+        recipientService.getRecipients().then(result => {
             this.setState({
-                routeList: result
+                recipients: result
+            })
+        })
+
+        routeService.getRoute(1).then(result => {
+            this.setState({
+                route: result
+            })
+        })
+
+        driverService.getDriver(params.driverId).then(result => {
+            this.setState({
+                driver: result
             })
         })
     }
 
-
-    displayAllRoutes() {
-        let routes = this.state.routeList
-        let str = "\n\nSomething "
-        for (let i = 0; i < routes.length; i++) {
-            str += routes[i].routes + " "
+    /**
+     * Function to return full name for individual recipients. Called 
+     * for each client in the itinerary for each driver's route.
+     * @param {Object} recipient Recipient object from the route.
+     * @returns Client's full name.
+     */
+    getRecipientName(recipient) {
+        let clients = this.state.recipients
+        for (let i = 0; i < clients.length; i++) {
+            if (clients[i].id === recipient.id) {
+                return clients[i].first_name + " " + clients[i].last_name
+            }
         }
-        return `Route:${this.state.routeList}\n ${str}`;
     }
 
-    displayAnyRoute() {
-        return `Blah:${this.state.routeList.routes}\n`
+    /**
+     * Function to return phone number for individual recipients. Called 
+     * for each client in the itinerary for the driver's route.
+     * @param {Object} recipient Recipient object from the route.
+     * @returns Client's phone number.
+     */
+    getPhone(recipient) {
+        let clients = this.state.recipients
+        for (let i = 0; i < clients.length; i++) {
+            if (clients[i].id === recipient.id) {
+                return clients[i].phone
+            }
+        }
     }
 
-
+    /**
+     * The render method used to display the component. 
+     * @returns The HTML to be rendered.
+     */
     render() {
         return (
-                <h1>
-                    {this.displayAnyRoute()}
-                    {this.state.routeList.length + "\n"}
-                    {this.state.routeList[0]}
-                    {this.displayAllRoutes()}
-                    {/* {this.state.routeList.map( r => 
-                        <p>{r.total_distance}</p>
-                    )} */}
-                </h1>
-        )        
+            <main className="content">
+                <div className="row">
+                <Container>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>City</th>
+                            <th>State</th>
+                            <th>Zip Code</th>
+                            <th>Phone Number</th>
+                            <th>Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.route.itinerary.map(l =>
+                            <tr>
+                                <td>{this.getRecipientName(l)}</td>
+                                <td>{l.address.address}</td>
+                                <td>{l.address.city}</td>
+                                <td>{l.address.state}</td>
+                                <td>{l.address.zipcode}</td>
+                                <td>{this.getPhone(l)}</td>
+                                <td>{l.demand}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </Container>
+                </div>
+            </main>
+        )
     }
-
-
-
-        //     <Container>
-        //         {this.state.routeList.routes.map( r =>
-        //         <Card border="dark" className="mb-4 mt-4">
-        //         <Card.Title className="card-header border-dark bg-grey">
-
-        //         </Card.Title>
-        //         <Card.Header className="pt-1 pl-1 pr-1 pb-1 border-dark bg-grey">
-        //         <Table className="hover table  mb-0">
-        //                 <thead>
-        //                     <tr>
-        //                         <th>Capacity</th>
-        //                         <th>Total Quantity</th>
-        //                         <th>Expected Distance Traveled (in Miles)</th>
-        //                         <th>Expected Duration (in Minutes)</th>
-        //                         <th>Itinerary</th>
-        //                     </tr>
-        //                 </thead>
-        //                 <tbody>
-        //                     <tr>
-        //                         <td>{this.getCapacity(r)}</td>
-        //                         <td>{r.total_quantity}</td>
-        //                         <td>{Math.round(r.total_distance)}</td>
-        //                         <td>{Math.round(r.total_duration)}</td>
-        //                         <td>{this.displayAllRoutes(r)}</td>
-        //                     </tr>
-        //                 </tbody>
-        //             </Table>
-        //         </Card.Header>
-        //         <Card.Subtitle className="pb-2 pt-4 pl-4 title">
-        //             <Col>
-        //                 <Row className="d-flex flex-row">
-        //                     <Col sm={8} className="title">
-        //                         Delivery List
-        //                     </Col>   
-        //                 </Row>
-        //             </Col> 
-        //         </Card.Subtitle>
-        //         <Card.Body className="card-body pl-1 pr-1 pt-1">
-        //             <Table className="hover table mb-0">
-        //                 <thead>
-        //                     <tr>
-        //                         <th>Name</th>
-        //                         <th>Address</th>
-        //                         <th>Apt #</th>
-        //                         <th>City</th>
-        //                         <th>State</th>
-        //                         <th>Zip Code</th>
-        //                         <th>Phone Number</th>
-        //                         <th>Quantity</th>
-        //                         <th>Comments</th>
-        //                     </tr>
-        //                 </thead>
-        //                 <tbody>
-        //             {r.itinerary.map( l =>                     
-        //                     <tr>
-        //                         <td>{this.getRecipientName(l)}</td>
-        //                         <td>{l.address.address}</td>
-        //                         <td>{this.getRecipientRoomNumber(l) ? this.getRecipientRoomNumber(l) : ""}</td>
-        //                         <td>{l.address.city}</td>
-        //                         <td>{l.address.state}</td>
-        //                         <td>{l.address.zipcode}</td>
-        //                         <td>{this.getPhone(l)}</td>
-        //                         <td>{l.demand}</td>
-        //                         <td>{this.getRecipientComment(l)}</td>
-        //                     </tr>
-        //                 )}
-        //                 </tbody>
-        //             </Table>
-        //         </Card.Body>
-        //         </Card>
-        //         )}            
-        // </Container>
-        //);
-
-}
-export default History;
+} export default History
