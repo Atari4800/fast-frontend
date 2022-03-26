fd47d68abb2286bf1c9c793c594b274631a2d3b4
Jake Peterson <60277102+Jake152@users.noreply.github.com>
2022-03-26 10:19:30 -0500

    updated-jake-develop Routing.js

--- frontend/src/components/Routing/Routing.js
+++ frontend/src/components/Routing/Routing.js
@@ -15,6 +15,7 @@ const  locationService  =  new  LocationService();
 const  routeService  =  new  RouteService();
 
 class  Routing  extends  Component {
+  
 
 constructor(props) {
      super(props);
@@ -31,8 +32,16 @@ constructor(props) {
          },
          loading: false,
          error: '',
-         errorMessage: ''
+         errorMessage: '',
+         errorDurationColor: '',
+         errorDeliveryColor: '',
+         'availability': {'sunday': true,
+         'monday': true, 'tuesday': true, 'wednesday': true, 
+         'thursday': true, 'friday': true, 'saturday': true},
+         isSelected: false
      };
+     this.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 
+                    'Friday', 'Saturday']
 
     //  this.getCenter = this.getCenter.bind(this);
      this.handleDriverCallback = this.handleDriverCallback.bind(this);
@@ -42,6 +51,10 @@ constructor(props) {
      this.handleDeliveryLimit = this.handleDeliveryLimit.bind(this);
      this.getEventValues = this.getEventValues.bind(this);
      this.handleSubmit = this.handleSubmit.bind(this);
+     this.handleAvailabilityChange = this.handleAvailabilityChange.bind(this);
+     this.getAvailability = this.getAvailability.bind(this);
+     this.selectAllDays = this.selectAllDays.bind(this);
+     this.deSelectAllDays = this.deSelectAllDays.bind(this);
 }
 
 componentDidMount() {
@@ -108,7 +121,9 @@ handleDeliveryLimit(event){
     route : {
     ...prevState.route,
     delivery_limit: value},
-    error: false
+    error: false,
+    errorDurationColor: '',
+    errorDeliveryColor: ''
   }));
   this.delivery_limit = value
 }
@@ -119,7 +134,9 @@ handleDuration(event){
     route : {
     ...prevState.route,
     duration_limit: value},
-    error: false
+    error: false,
+    errorDurationColor: '',
+    errorDeliveryColor: ''
   }));
   this.duration_limit = value
 }
@@ -177,25 +194,89 @@ handleSubmit = (event) => {
   else if (!(this.delivery_limit) && !(this.duration_limit)) {
     this.setState({
       error: true,
-      errorMessage: 'ERROR: Delivery Limit and Duration are empty'
+      errorMessage: 'ERROR: Delivery Limit and Duration are empty',
+      errorDurationColor: 'red',
+      errorDeliveryColor: 'red'
     })
   }
   else if (!(this.duration_limit)) {
     this.setState({
       error: true,
-      errorMessage: 'ERROR: Duration is empty'
+      errorMessage: 'ERROR: Duration is empty',
+      errorDurationColor: 'red'
     })
   }
   else {
     this.setState({
       error: true,
-      errorMessage: 'ERROR: Delivery Limit is empty'
+      errorMessage: 'ERROR: Delivery Limit is empty',
+      errorDeliveryColor: 'red'
     })
   }
 }
 
+handleAvailabilityChange(event) {
+  let [value, name] = this.getEventValues(event)
+  let id = event.target.id
+  let checked = event.target.checked 
+  this.setState(prevState => ({
+          [name]: {
+               ...prevState[name],
+               [id.toLowerCase()]: checked
+          }    
+      }
+      ));
+}
+
+getAvailability(day) {
+  day = day.toLowerCase()
+  return this.state.availability[day] ;
+}
+
+selectAllDays = (event) => {
+  let name = "availability"
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let checked = true
+  for (let i = 0; i < days.length; i++) {
+    let id = days[i]
+    this.setState(prevState => ({
+      [name]: {
+        ...prevState[name],
+        [id.toLowerCase()]: checked
+      }
+    }));
+  }
+  this.setState({
+    isSelected: false
+  })
+}
+
+deSelectAllDays = (event) => {
+  let name = "availability"
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let checked = false
+  for (let i = 0; i < days.length; i++) {
+    let id = days[i]
+    this.setState(prevState => ({
+      [name]: {
+        ...prevState[name],
+        [id.toLowerCase()]: checked
+      }
+    }));
+  }
+  this.setState({
+    isSelected: true
+  })
+}
+
+onSelect(event){
+  //passing driver id to parent component in Routing.js
+  this.props.parentCallback(event);
+}
+
+
 render() {
-     
+
     const { handleSubmit, state } = this;
 
     return (
@@ -204,14 +285,14 @@ render() {
         <Row className="mt-4">
         <Form.Group as={Col} controlId="formGridDeliveryLimit">
           <Form.Label className="title">Delivery Limit</Form.Label>
-          <Form.Control type="number" placeholder="Driver Delivery Limit"
+          <Form.Control type="number" placeholder="Driver Delivery Limit" style={{ borderColor: this.state.errorDeliveryColor }}
                         required onChange={this.handleDeliveryLimit} name="delivery_limit" min="1"/>
         </Form.Group>
         <Form.Group as={Col} controlId="formGridDurationLimit">
           <Form.Label className="title">Duration</Form.Label>
-          <Form.Control type="number" placeholder="Duration Limit in Hours"
+          <Form.Control type="number" placeholder="Duration Limit in Hours" style={{ borderColor: this.state.errorDurationColor }} 
                         required onChange={this.handleDuration} name="duration_limit" min="1"/>
-        </Form.Group>
+        </Form.Group> 
         <Form.Group as={Col} controlId="formGridDeparture">
           <Form.Label className="title">Departure Location</Form.Label>
           <Form.Select value={this.state.route.departure.location.address} 
@@ -227,22 +308,34 @@ render() {
 
           </Form.Select>
         </Form.Group>
-        </Row>   
+        </Row> 
+
+        <Form.Group className="mb-3" style={{ marginTop: 20 }}>
+            <Row><Form.Label className="title">Driver Availability</Form.Label></Row>
+            <Button className="mr-4 btn" variant="primary" onClick={this.state.isSelected ? this.selectAllDays : this.deSelectAllDays}>
+              Select/Deselect All</Button>
+              { this.days.map( d =>
+                <Form.Check type="checkbox" name="availability" value="true"
+                checked={this.getAvailability(d)}
+                inline label={d} id={d}
+                onChange={this.handleAvailabilityChange}
+                />)}
+        </Form.Group> 
           
         <br/>
         <SelectDriver parentCallback = {this.handleDriverCallback}/>
         <SelectRecipient parentCallback = {this.handleRecipientCallback} />
        
         <Button className="mr-2 mt-4 btn" variant="primary" disabled={this.state.loading}
-               onClick={handleSubmit}>
-                 {this.state.loading ?  	
-                   <Spinner	
-                     animation="border" role="status">	
-                     <span className="visually-hidden">Loading...</span>	
-                   </Spinner> : "Create Route"}	
+                onClick={handleSubmit}>
+                  {this.state.loading ?  
+                    <Spinner
+                      animation="border" role="status">
+                      <span className="visually-hidden">Loading...</span>
+                    </Spinner> : "Create Route"}
         </Button>
-        {this.state.error ? 	
-                    <h3 className='error' style={{fontSize: 20, color: "red", marginTop: 10}}> { this.state.errorMessage } </h3> : ""}
+        {this.state.error ? 
+                    <h3 className='error' style={{ fontSize: 20, color: "red", marginTop: 10 }}> { this.state.errorMessage } </h3> : ""}
         </Form> 
       </Container>
     );
